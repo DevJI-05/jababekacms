d10a5cbcf23c863765c741841c437ff251dded9a
DevJI-05 <git.dev5@jababeka-infra.com>
2026-08-27 11:03:09 +0700

    feat: profile page

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile(isSimple: false)
             ->colors([
                 'primary' => Color::hex('#175A3A'),
             ])
